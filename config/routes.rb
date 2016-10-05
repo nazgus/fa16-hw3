f757784d983b48188dde877171b561b834be8e6d
nazgus <gus@deleon.org.mx>
2016-10-05 08:57:14 -0700

    Redirect root to student form

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   post '/teachers', to: 'teachers#create'
   get '/students/new', to: 'students#new'
   post '/students', to: 'students#create'
+  root :to => redirect('/students/new')
 end
